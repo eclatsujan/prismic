4feca2737592c25960ecf5eb47d87416d07d1a5d
Prawesh Panthi <praweshpanthi@gmail.com>
2021-03-15 13:43:28 +0545

    some changes

--- config/index.js
+++ config/index.js
@@ -1,8 +1,4 @@
 export default {
-<<<<<<< HEAD
   URL: "https://npm.resham.info.np",
-=======
-  URL: process.env.SERVER_URL,
->>>>>>> 3aef4712393ba17c8a0bd46ea80b1ec1562fbd7b
   REACT_APP_MAILCHIMP_URL: "",
 };
