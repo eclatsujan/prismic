e664e5b51dd3c9d1c0bea4cfa58db2bc73b0d8c0
Prawesh Panthi <praweshpanthi@gmail.com>
2021-03-15 14:22:49 +0545

    deploymenty configs

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 export default {
-  URL: "https://npm.resham.info.np",
+  URL: process.env.SERVER_URL,
   REACT_APP_MAILCHIMP_URL: "",
 };
--- next.config.js
+++ next.config.js
@@ -1,23 +1,25 @@
-const webpack = require('webpack')
+const webpack = require("webpack");
 
 module.exports = {
-    webpack(config) {
-        config.plugins.push(
-            new webpack.ProvidePlugin({
-              '$': 'jquery',
-              jQuery: 'jquery',
-              "window.jQuery": "jquery"
-            })
-        );
-        // config.module.rules.push({
-        //     test: /\.(png|jpg|gif|svg)$/,
-        //         use: {
-        //             loader: 'url-loader',
-        //             options: {
-        //                 limit: 100000
-        //             }
-        //         }
-        // });
-      return config;
-    },
+  // Target must be serverless
+  target: "serverless",
+  webpack(config) {
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery",
+      })
+    );
+    // config.module.rules.push({
+    //     test: /\.(png|jpg|gif|svg)$/,
+    //         use: {
+    //             loader: 'url-loader',
+    //             options: {
+    //                 limit: 100000
+    //             }
+    //         }
+    // });
+    return config;
+  },
 };
